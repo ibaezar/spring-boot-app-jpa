828962701b96f8d9fca8ab34618e51a07dbdb57c
Izhar Baeza <ibaezar.1@gmail.com>
2022-11-09 23:24:35 -0300

    starter project

---
+++ src/main/java/com/ibaezar/springboot/jpa/app/SpringBootAppJpaApplication.java
@@ -0,0 +1,13 @@
+package com.ibaezar.springboot.jpa.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAppJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAppJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ibaezar/springboot/jpa/app/SpringBootAppJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibaezar.springboot.jpa.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAppJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
