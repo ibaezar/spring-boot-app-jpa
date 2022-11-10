26210e86d4b8419de18dedabb6e197b535c50565
Izhar Baeza <ibaezar.1@gmail.com>
2022-11-10 13:09:23 -0300

    file import.sql for import client list created

---
+++ src/main/resources/import.sql
@@ -0,0 +1,5 @@
+INSERT INTO clients (name, lastname, email, created_at) VALUES ('Izhar', 'Baeza', 'ibaezar@outlook.com', '2022-11-10');
+INSERT INTO clients (name, lastname, email, created_at) VALUES ('Liam', 'Baeza', 'liam.baeza@gmail.com', '2022-11-10');
+INSERT INTO clients (name, lastname, email, created_at) VALUES ('Valentina', 'García', 'v.garcia@gmail.com', '2022-11-10');
+INSERT INTO clients (name, lastname, email, created_at) VALUES ('Victor', 'Baeza', 'victorb@gmail.com', '2022-11-10');
+INSERT INTO clients (name, lastname, email, created_at) VALUES ('Levi', 'Baeza', 'levi.b.r@gmail.com', '2022-11-10');
