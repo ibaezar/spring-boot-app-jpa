a95f1b6e4e93769db9678771f0f35a462cc8d164
Izhar Baeza <ibaezar.1@gmail.com>
2022-12-02 15:31:18 -0300

    configuration in MvcConfig for multilenguage

--- src/main/java/com/ibaezar/springboot/jpa/app/MvcConfig.java
+++ src/main/java/com/ibaezar/springboot/jpa/app/MvcConfig.java
@@ -1,9 +1,16 @@
 package com.ibaezar.springboot.jpa.app;
 
+import java.util.Locale;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
@@ -27,5 +34,25 @@ public class MvcConfig implements WebMvcConfigurer {
     //TODO -> Metodo viewController para personalizar vista de error Acceso denegado de spring security
     public void addViewControllers(ViewControllerRegistry registry){
         registry.addViewController("/error_403").setViewName("error_403");
+    }
+
+    
+    //TODO -> Configuración para multilenguaje
+    @Bean
+    public LocaleResolver localeResolver(){
+        SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+        localeResolver.setDefaultLocale(new Locale("es", "ES"));
+        return localeResolver;
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor(){
+        LocaleChangeInterceptor localeInterceptor = new LocaleChangeInterceptor();
+        return localeInterceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
     }
 }
